0a59108971c634304de4ce516373353cf16b4a6d
Md Irfan <rn555666777@gmail.com>
2026-04-28 09:47:39 +0530

    feat: add SPA-compatible about page content

--- src/js/loader.js
+++ src/js/loader.js
@@ -4,3 +4,10 @@ export const loadComponent = async (id, file) => {
 
   document.getElementById(id).innerHTML = html;
 };
+
+export async function loadPage(page) {
+  const res = await fetch(`/pages/${page}.html`);
+  const html = await res.text();
+
+  document.querySelector("main").innerHTML = html;
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-import { loadComponent } from "./loader.js";
+import { loadComponent, loadPage } from "./loader.js";
 import { setupThemeToggle } from "./theme.js";
 
 import "./dashboard.js";
@@ -10,5 +10,20 @@ async function init() {
 
   setupThemeToggle();
 }
+
+document.addEventListener("click", (e) => {
+  const link = e.target.closest("a[data-page]");
+  if (!link) return;
+
+  e.preventDefault();
+
+  const page = link.dataset.page;
+
+  if (page === "home") {
+    location.reload();
+  } else {
+    loadPage(page);
+  }
+});
 
 init();
